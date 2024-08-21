4ac2134c3a3a84ee934fa2f3d35c3becfe0aecb7
0xShawn <intelbeyondrepair@gmail.com>
2024-08-21 04:40:21 -0700

    Add project files.

---
+++ Scuzzy/LTexture.hpp
@@ -0,0 +1,36 @@
+#include <SDL.h>
+#include <string>
+
+extern SDL_Renderer* gRenderer;
+
+//Texture wrapper class
+class LTexture
+{
+public:
+    //Initializes variables
+    LTexture();
+
+    //Deallocates memory
+    ~LTexture();
+
+    //Loads image at specified path
+    bool loadFromFile(std::string path);
+
+    //Deallocates texture
+    void free();
+
+    //Renders texture at given point
+    void render(int x, int y, SDL_Rect* clip = NULL);
+
+    //Gets image dimensions
+    int getWidth();
+    int getHeight();
+
+private:
+    //The actual hardware texture
+    SDL_Texture* mTexture;
+
+    //Image dimensions
+    int mWidth;
+    int mHeight;
+};
