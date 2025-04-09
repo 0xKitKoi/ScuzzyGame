49759dd18a58f2b7ecca821e9ff1125b2a23eabb
0xShawn <intelbeyondrepair@gmail.com>
2025-04-09 07:51:06 -0700

    Added a Windows Build System

---
+++ premake5.lua
@@ -0,0 +1,69 @@
+workspace "Scuzzy"
+   architecture "x64"
+   configurations { "Debug", "Release" }
+
+   -- SDL versions
+   sdl2_version = "2.32.4"
+   sdl2_image_version = "2.8.8"
+   sdl2_ttf_version = "2.24.0"
+
+project "Scuzzy"
+   location "Scuzzy"
+   kind "ConsoleApp"
+   language "C++"
+   cppdialect "C++17"
+   staticruntime "off"
+
+   targetdir ("bin/%{cfg.buildcfg}")
+   objdir ("bin-int/%{cfg.buildcfg}")
+
+   files {
+      "Scuzzy/Source/**.h",
+      "Scuzzy/Source/**.hpp",
+      "Scuzzy/Source/**.cpp"
+   }
+
+   includedirs {
+      "Scuzzy",
+      "thirdparty/SDL2/include",
+      "thirdparty/SDL2_image/include",
+      "thirdparty/SDL2_ttf/include"
+   }
+
+   libdirs {
+      "thirdparty/SDL2/lib/x64",
+      "thirdparty/SDL2_image/lib/x64",
+      "thirdparty/SDL2_ttf/lib/x64"
+   }
+
+   links {
+      "SDL2",
+      "SDL2main",
+      "SDL2_image",
+      "SDL2_ttf"
+   }
+
+ filter "system:windows"
+   systemversion "latest"
+   defines { "PLATFORM_WINDOWS" }
+
+   postbuildcommands {
+      -- Copy SDL2.dll
+      "{COPY} %{wks.location}/thirdparty/SDL2/lib/x64/SDL2.dll %{cfg.targetdir}",
+      -- Copy SDL2_image.dll
+      "{COPY} %{wks.location}/thirdparty/SDL2_image/lib/x64/SDL2_image.dll %{cfg.targetdir}",
+      -- Copy SDL2_ttf.dll
+      "{COPY} %{wks.location}/thirdparty/SDL2_ttf/lib/x64/SDL2_ttf.dll %{cfg.targetdir}"
+   }
+
+
+
+
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
